8b380aea93bc6eacaacc1030a99d55ab2bb85498
kyle-gilmartin <kylegilmartin8@gmail.com>
2021-06-07 16:20:59 +0100

    userprofile image click no has user permissions

--- app/src/main/java/edu/kylegilmartin/shopapp/LoginRegister/UserProfileActivity.kt
+++ app/src/main/java/edu/kylegilmartin/shopapp/LoginRegister/UserProfileActivity.kt
@@ -1,13 +1,20 @@
 package edu.kylegilmartin.shopapp.LoginRegister
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import edu.kylegilmartin.shopapp.R
 import edu.kylegilmartin.shopapp.models.User
 import edu.kylegilmartin.shopapp.widgets.Constants
+import edu.kylegilmartin.shopapp.widgets.popupActivity
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.util.jar.Manifest
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : popupActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
@@ -22,5 +29,56 @@ class UserProfileActivity : AppCompatActivity() {
         et_last_name.setText(userDetails.lastName)
         et_email.isEnabled =false
         et_email.setText(userDetails.email)
+
+        iv_user_photo.setOnClickListener(this@UserProfileActivity)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_user_photo -> {
+                    // check if the permission is already allowed or we need to request for it.
+                    // check the READ_EXTERNAL_STORAGE permission and if it is not allowed we will request for the same.
+                    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        showErrorSnackBar("You already have the storage permission.", false)
+                    } else {
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+                        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    /*This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
+
+     @param requestCode
+    * @param permissions
+    * @param grantResults
+    */
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                showErrorSnackBar("The storage permission is granted.", false)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                        this,
+                        resources.getString(R.string.read_storage_permission_denied),
+                        Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
--- app/src/main/java/edu/kylegilmartin/shopapp/widgets/Constants.kt
+++ app/src/main/java/edu/kylegilmartin/shopapp/widgets/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val SHOP_PREFERENCES: String = "ShopPrefs"
     const val LOGGED_IN_USERNAME:String = "Logged_in_username"
     const val EXTRA_USER_DETAILS: String = "extra_user_details"
+    const val READ_STORAGE_PERMISSION_CODE = 2
 }
