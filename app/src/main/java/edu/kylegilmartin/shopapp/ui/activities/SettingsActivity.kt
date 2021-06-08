ff1c067a73dd00e80ef8c5b18e78b4bafb6768ae
kyle-gilmartin <kylegilmartin8@gmail.com>
2021-06-08 13:20:20 +0100

    added the settings menu to the DashboardFragment.kt

---
+++ app/src/main/java/edu/kylegilmartin/shopapp/ui/activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package edu.kylegilmartin.shopapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.kylegilmartin.shopapp.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
--- app/src/main/java/edu/kylegilmartin/shopapp/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/edu/kylegilmartin/shopapp/ui/fragments/DashboardFragment.kt
@@ -1,16 +1,23 @@
 package edu.kylegilmartin.shopapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import edu.kylegilmartin.shopapp.R
+import edu.kylegilmartin.shopapp.ui.activities.SettingsActivity
 
 class DashboardFragment : Fragment() {
 
     //private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +29,25 @@ class DashboardFragment : Fragment() {
         val textView: TextView = root.findViewById(R.id.text_dashboard)
             textView.text = "dashborad"
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when (id) {
+
+            R.id.action_settings -> {
+
+                startActivity(Intent(activity, SettingsActivity::class.java))
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
